db051597f24ed56103d5aca2613c1403e05708a3
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-12-08 23:37:10 +0200

    Minor changes

--- app/reminder/services.py
+++ app/reminder/services.py
@@ -1,13 +1,32 @@
+import time
+
 from service import Service
 
-import time
+import logging
 
 
 class ReminderService(Service):
 
 	def run(self):
+		logger = logging.getLogger()
+		logger.setLevel(logging.INFO)
+
+		# create a file handler
+		handler = logging.FileHandler('./log.txt')
+		handler.setLevel(logging.INFO)
+
+		# create a logging format
+		formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+		handler.setFormatter(formatter)
+
+		# add the handlers to the logger
+		logger.addHandler(handler)
+
 		i = 0
 		while i < 5:
-			print('Hello {}'.format(i))
 			i += 1
-			time.sleep(5)
+
+			# TODO: send notification
+			logger.info('Service info: {}'.format(i))
+
+			time.sleep(2)
