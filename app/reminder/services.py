f4a6882a8d1de1aaaba1a2e8deded4b85e46c79c
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-12-10 10:51:12 +0200

    Add daemon_service.py

--- app/reminder/services.py
+++ app/reminder/services.py
@@ -1,26 +1,37 @@
 import time
 
-from service import Service
+from app.reminder.daemon_service import Service
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+def notification(msg):
+	w = QWidget()
+	w.resize(250, 150)
+	w.move(300, 300)
+	w.setWindowTitle(msg)
+	return w
 
 
 class ReminderService(Service):
 
 	def run(self):
 		try:
-			i = 0
-			while i < 5 and not self.got_sigterm():
+			while not self.got_sigterm():
 				try:
-					i += 1
+					time.sleep(5)
+
+					app = QApplication(sys.argv)
+					n = notification('Hello World!')
+					n.show()
+					sys.exit(app.exec_())
 
 					# TODO: send notification
 
-					with open('./file.txt', 'a') as the_file:
-						the_file.write('Service info: {}\n'.format(i))
-
-					time.sleep(5)
 				except Exception as exc:
 					with open('./errors_file.txt', 'a') as the_file:
 						the_file.write('Service error: {}\n'.format(exc))
 		except Exception as exc:
-			with open('./errors_file.txt', 'a') as the_file:
-				the_file.write('Service error: {}\n'.format(exc))
+			with open('./fatal_errors_file.txt', 'a') as the_file:
+				the_file.write('Fatal error: {}\n'.format(exc))
