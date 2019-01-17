76a0443eb285da554a7d2289586efa90f4fd655f
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2019-01-17 17:25:35 +0200

    add settings tab view

--- app/ui/widgets/forms/settings.py
+++ app/ui/widgets/forms/settings.py
@@ -1,5 +1,7 @@
+from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QListWidgetItem
 
 from app.ui.utils import create_button
 
@@ -9,16 +11,34 @@ class SettingsForm:
 	def __init__(self, parent):
 		self.parent = parent
 		self.parent.setFixedSize(500, 400)
+		self.parent.setWindowTitle('Settings')
 		self.calendar = None
-
 		self.setup_ui()
 
 	def setup_ui(self):
 		content = QVBoxLayout()
 
-		# TODO: implement settings content
+		settings_general_tabs = QTabWidget(self.parent)
+
+		settings_general_tabs.setMinimumWidth(self.parent.width() - 22)
+
+		app_tab = QWidget(flags=settings_general_tabs.windowFlags())
+		notifications_tab = QWidget(flags=settings_general_tabs.windowFlags())
+		settings_general_tabs.addTab(app_tab, 'App')
+		settings_general_tabs.addTab(notifications_tab, 'Notifications')
+
+		content.addWidget(settings_general_tabs, alignment=Qt.AlignLeft)
+
+		self.set_app_settings(content)
+		self.set_user_settings(content)
 
 		self.parent.setLayout(content)
+
+	def set_app_settings(self, content):
+		pass
+
+	def set_user_settings(self, content):
+		pass
 
 	def set_calendar_widget(self, calendar):
 		self.calendar = calendar
