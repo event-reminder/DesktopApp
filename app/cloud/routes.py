a7e2797d3fcc25d5c999430bc60e84e9522f790c
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2019-02-18 01:07:28 +0200

    Updated routes

--- app/cloud/routes.py
+++ app/cloud/routes.py
@@ -5,15 +5,21 @@ _BASE = 'http://{}:{}'.format(_HOST, _PORT)
 
 _API = '{}/api/v1'.format(_BASE)
 
-REGISTER = '{}/accounts/'.format(_API)
-ACCOUNTS_EDIT = '{}/edit'.format(REGISTER)
-
 _AUTH = '{}/auth/'.format(_API)
 
-LOGIN = '{}login/'.format(_AUTH)
-LOGOUT = '{}logout/'.format(_AUTH)
-USER = '{}user/'.format(_AUTH)
+AUTH_LOGIN = '{}login/'.format(_AUTH)
+AUTH_LOGOUT = '{}logout/'.format(_AUTH)
+
+_ACCOUNTS = '{}/accounts'.format(_API)
+
+ACCOUNT_EDIT = '{}/edit'.format(_ACCOUNTS)
+ACCOUNT_DETAILS = '{}/user'.format(_ACCOUNTS)
+ACCOUNT_CREATE = '{}/create'.format(_ACCOUNTS)
+ACCOUNT_DELETE = '{}/delete'.format(_ACCOUNTS)
+ACCOUNT_SEND_TOKEN = '{}/send/token'.format(_ACCOUNTS)
+ACCOUNT_PASSWORD_RESET = '{}/password/reset'.format(_ACCOUNTS)
 
 BACKUPS = '{}/backups/'.format(_API)
-BACKUPS_CREATE = '{}create'.format(BACKUPS)
+BACKUP_CREATE = '{}create'.format(BACKUPS)
+BACKUP_DELETE = '{}delete/'.format(BACKUPS)
 BACKUP_DETAILS = '{}details/'.format(BACKUPS)
