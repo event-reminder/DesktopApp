6226938ba6175b9493093fde3b52c2481436a275
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2019-03-24 13:55:26 +0200

    Updated release date constant

--- erdesktop/__init__.py
+++ erdesktop/__init__.py
@@ -4,7 +4,7 @@ import os
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
-APP_VERSION = '2019.0.2-alpha-1'
+APP_VERSION = '2019.0.3-alpha'
 
 PY_PACKAGE_NAME = APP_ROOT.split('/')[-1]
 
@@ -14,4 +14,4 @@ APP_NAME = 'Event Reminder'
 
 APP_ORGANIZATION = 'YuriyLisovskiy'
 
-APP_RELEASE_DATE = 'February 2, 2019'
+APP_RELEASE_DATE = 'March 24, 2019'
