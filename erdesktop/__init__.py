194bb6c1c28ebef02783cfbff2f142af03841223
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2019-03-25 01:29:46 +0200

    Added resources to setup.py script

--- erdesktop/__init__.py
+++ erdesktop/__init__.py
@@ -4,7 +4,7 @@ import os
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
-APP_VERSION = '2019.0.3-alpha-1'
+APP_VERSION = '2019.0.2-beta-1'
 
 PY_PACKAGE_NAME = APP_ROOT.split('/')[-1]
 
--- setup.py
+++ setup.py
@@ -4,10 +4,24 @@
 import io
 import os
 import platform
+import subprocess
 
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 
 from erdesktop import APP_VERSION, PY_PACKAGE_NAME, APP_DESCRIPTION
+
+
+class PostInstallCommand(install):
+
+	def run(self):
+		if 'Windows' in platform.system():
+			root = os.path.dirname(os.path.abspath(__file__))
+			win_libs = open('{}/windows_requirements.txt'.format(root), 'r').read().split('\n')
+			while '' in win_libs:
+				win_libs.remove('')
+			subprocess.call('pip install'.split() + win_libs)
+		install.run(self)
 
 
 URL = 'https://github.com/YuriyLisovskiy/EventReminder'
@@ -17,13 +31,8 @@ REQUIRES_PYTHON = '>=3.6.0'
 
 REQUIRED = open('requirements.txt', 'r').read().split('\n')
 
-if 'Windows' in platform.system():
-	REQUIRED += open('windows_requirements.txt', 'r').read().split('\n')
-
 while '' in REQUIRED:
 	REQUIRED.remove('')
-
-print(REQUIRED)
 
 try:
 	with io.open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md')) as f:
@@ -40,6 +49,9 @@ setup(
 			PY_PACKAGE_NAME, PY_PACKAGE_NAME, 'app_main'
 		)]
 	},
+	cmdclass={
+		'install': PostInstallCommand,
+	},
 	long_description=long_description,
 	long_description_content_type='text/markdown',
 	author=AUTHOR,
@@ -50,6 +62,9 @@ setup(
 		PY_PACKAGE_NAME: ['tmp/']
 	},
 	packages=find_packages(exclude=('tests',)),
+	package_data={
+		'erdesktop': ['resources/images/ico/*.ico', 'resources/images/png/*.png']
+	},
 	install_requires=REQUIRED,
 	include_package_data=True,
 	license='GPLv3',
